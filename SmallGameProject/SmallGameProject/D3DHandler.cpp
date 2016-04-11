f383e00c03f179aba6394e1684c55d996166e64f
relinkz <seb.lundgren@gmail.com>
2016-04-11 15:18:30 +0200

    fixed syntaxerror in AI.h and made a createDevice

--- SmallGameProject/SmallGameProject/D3DHandler.cpp
+++ SmallGameProject/SmallGameProject/D3DHandler.cpp
@@ -1,1 +1,45 @@
 #include "D3DHandler.h"
+
+D3DHandler::D3DHandler()
+{
+	this->clientDriverType = D3D_DRIVER_TYPE_HARDWARE;
+	this->featureSupport = D3D_FEATURE_LEVEL_11_0;
+}
+
+D3DHandler::~D3DHandler()
+{
+}
+
+bool D3DHandler::createDeviceAndContext()
+{
+	HRESULT result;
+	D3D_FEATURE_LEVEL featureLevel;
+	HRESULT hr = D3D11CreateDevice(
+		0,							// default adapter
+		this->clientDriverType,		// D3D_DRIVER_TYPE_HARDWARE, created in the default constructor
+		0,							// no software device
+		D3D11_CREATE_DEVICE_DEBUG,	// flag
+		0, 
+		0,						// default feature level array
+		D3D11_SDK_VERSION,
+		&md3dDevice,
+		&featureLevel,
+		&md3dImmediateContext);
+
+	if (FAILED(hr))
+	{
+		MessageBox(0, L"D3D11CreateDevice Failed.", 0, 0);
+		return false;
+	}
+
+	if (featureLevel != D3D_FEATURE_LEVEL_11_0)
+	{
+		MessageBox(0, L"Direct3D Feature Level 11 unsupported.", 0, 0);
+		return false;
+	}
+
+
+
+	result = D3D11CreateDevice();
+}
+
--- SmallGameProject/SmallGameProject/D3DHandler.h
+++ SmallGameProject/SmallGameProject/D3DHandler.h
@@ -1,12 +1,42 @@
 #ifndef D3DHANDLER_H
 #define D3DHANDLER_H
 
-class D3DHandler {
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
 
-private:
+#include <D3D11.h>
+#include <windows.h>
 
+
+class D3DHandler 
+{
 public:
+	D3DHandler();
+	virtual ~D3DHandler();
 
+protected:
+	bool InitDirect3D();
+	bool createDeviceAndContext();
+
+	void CalculateFrameStats();
+protected:
+	ID3D11Device* gDevice;
+	ID3D11DeviceContext* gDeviceContext;
+	IDXGISwapChain* gSwapChain;
+
+	//data to depthStencil
+	ID3D11Texture2D* mDepthStencilBuffer;
+	ID3D11RenderTargetView* mDepthStencilRTV;
+	ID3D11DepthStencilView* mDepthStencilView;
+
+	D3D11_VIEWPORT gameViewport;
+
+	D3D_DRIVER_TYPE clientDriverType;
+	D3D_FEATURE_LEVEL featureSupport;
+	int clientWidth;
+	int clientHeight;
+
+	bool msaa4xEnabled;
 };
 
 #endif
