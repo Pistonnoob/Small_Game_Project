dcb55f39c8db26c77a1f2cf7b65848632e208a92
relinkz <seb.lundgren@gmail.com>
2016-04-11 16:19:58 +0200

    changes

--- SmallGameProject/SmallGameProject/D3DHandler.h
+++ SmallGameProject/SmallGameProject/D3DHandler.h
@@ -1,12 +1,51 @@
 #ifndef D3DHANDLER_H
 #define D3DHANDLER_H
 
-class D3DHandler {
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
 
-private:
+#include <D3D11.h>
+#include <windows.h>
+#include <string>
 
+using namespace std;
+
+
+class D3DHandler 
+{
 public:
+	D3DHandler();
+	virtual ~D3DHandler();
 
+protected:
+	bool InitDirect3D();
+
+	void createDeviceAndContext() throw(...);
+	// checks the quality level support for 4x MSAA
+	void check4xMsaaQualitySupp() throw(...);
+	void describeSwapChain() throw(...);
+
+	void CalculateFrameStats();
+protected:
+	ID3D11Device* gDevice;
+	ID3D11DeviceContext* gDeviceContext;
+	IDXGISwapChain* gSwapChain;
+
+	//data to depthStencil
+	ID3D11Texture2D* mDepthStencilBuffer;
+	ID3D11RenderTargetView* mDepthStencilRTV;
+	ID3D11DepthStencilView* mDepthStencilView;
+
+	D3D11_VIEWPORT gameViewport;
+
+	D3D_DRIVER_TYPE clientDriverType;
+	D3D_FEATURE_LEVEL featureSupport;
+	
+	int clientWidth;
+	int clientHeight;
+
+	UINT m4xMsaaQuality;
+	bool msaa4xEnabled;
 };
 
 #endif
