fdf9725bbd2d9caec4db499f4fc907e597104ae6
relinkz <seb.lundgren@gmail.com>
2016-04-11 16:11:31 +0200

    adding a createSwapChainDesc function

--- SmallGameProject/SmallGameProject/D3DHandler.cpp
+++ SmallGameProject/SmallGameProject/D3DHandler.cpp
@@ -81,3 +81,36 @@ void D3DHandler::check4xMsaaQualitySupp() throw(...)
 	assert(m4xMsaaQuality > 0);
 }
 
+void D3DHandler::describeSwapChain() throw(...)
+{
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	swapChainDesc.BufferDesc.Width = this->clientWidth;
+	swapChainDesc.BufferDesc.Height = this->clientHeight;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;	//pc master-race
+	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	//are we using MSAA?
+	if (this->msaa4xEnabled)
+	{
+		swapChainDesc.SampleDesc.Count = 4;
+		swapChainDesc.SampleDesc.Quality = this->m4xMsaaQuality - 1;
+	}
+	//if no MSAA
+	else
+	{
+		swapChainDesc.SampleDesc.Count = 1;
+		swapChainDesc.SampleDesc.Quality = 0;
+	}
+	
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.OutputWindow;
+	swapChainDesc.Windowed = true;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapChainDesc.Flags = 0;
+	
+}
+
