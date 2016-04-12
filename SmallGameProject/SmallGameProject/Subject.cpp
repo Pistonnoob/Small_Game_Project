a449efc5a955481d27fa4abacc2fce77e16ead66
Tobbe <tobbe.kling@hotmail.com>
2016-04-12 14:34:29 +0200

    implemented Subject::notify

--- SmallGameProject/SmallGameProject/Subject.cpp
+++ SmallGameProject/SmallGameProject/Subject.cpp
@@ -1,5 +1,17 @@
 #include "Subject.h"
 
+void Subject::notify(const Subject subject, Events::ENTITY evnt)
+{
+	//Send the event to all the observers
+	std::set<Observer*>::iterator it;										//Create the iterator
+	
+	for (it = this->observers.begin(); it != this->observers.end(); it++) {	//Iterate
+		
+		Observer* e = *it;													//Create a pointer and set it to the object the iterator is pointing to
+		e->onNotify(subject, evnt);											//Notify
+	}
+}
+
 void Subject::addObserver(Observer* observer)
 {
 	this->observers.insert(observer);
--- SmallGameProject/SmallGameProject/Subject.h
+++ SmallGameProject/SmallGameProject/Subject.h
@@ -8,6 +8,7 @@ class Subject {
 
 private:
 	std::set<Observer*> observers;
+	void notify(const Subject subject, Events::ENTITY entity);
 
 public:
 	void addObserver(Observer* observer);
