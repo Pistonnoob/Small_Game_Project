515f3a20d58e4c6789617ca8a85a1dbb289b5b7f
Oscar Engborg <Engborg@live.se>
2016-04-12 11:41:45 +0200

    Implemented ScreenQuad class

--- SmallGameProject/SmallGameProject/GraphicHandler.h
+++ SmallGameProject/SmallGameProject/GraphicHandler.h
@@ -1,12 +1,19 @@
 #ifndef GRAPHICHANDLER_H
 #define GRAPHICHANDLER_H
-#include "D3DHandler.h"
 
+#include "D3DHandler.h"
+#include "DeferredShaderHandler.h"
+#include "LightShaderHandler.h"
+#include "ScreenQuad.h"
 
 class GraphicHandler 
 {
 protected:
 	D3DHandler* engine;
+	DeferredShaderHandler* deferredShaderH;
+	LightShaderHandler* lightShaderH;
+	ScreenQuad* screenQuad;
+
 public:
 	GraphicHandler();
 	GraphicHandler(HWND &window);
