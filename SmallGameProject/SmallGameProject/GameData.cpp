bd0a34bdd9dc1d00535179c83a5cd7ce7ec491c6
Tobbe <tobbe.kling@hotmail.com>
2016-04-15 10:24:55 +0200

    Implemented the GameData.h/cpp as a singleton. Need to add what data to store

--- SmallGameProject/SmallGameProject/GameData.cpp
+++ SmallGameProject/SmallGameProject/GameData.cpp
@@ -1,1 +1,28 @@
 #include "GameData.h"
+GameData::GameData()
+{
+
+}
+
+GameData::~GameData()
+{
+	isInstatiated = false;
+	delete this->single;
+}
+
+GameData* GameData::getInstance()
+{
+	if (!isInstatiated) {
+		single = new GameData();
+		isInstatiated = true;
+		return single;
+	}
+	else {
+		return single;
+	}
+}
+
+void GameData::onNotify(const Entity& entity, Events::ENTITY evnt)
+{
+
+}
--- SmallGameProject/SmallGameProject/GameData.h
+++ SmallGameProject/SmallGameProject/GameData.h
@@ -6,9 +6,24 @@
 class GameData : public Observer {
 
 private:
+	static bool isInstatiated;	//Check flag
+	static GameData* single;
+
+	GameData();					//Only GameData will create itself
+
+	//Data
+
+
 
 public:
+	
+	~GameData();
+	static GameData* getInstance();
+	virtual void onNotify(const Entity& entity, Events::ENTITY evnt);
 
 };
+
+bool GameData::isInstatiated = false;
+GameData* GameData::single = nullptr;
 
 #endif
