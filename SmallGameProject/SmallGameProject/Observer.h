defd579fa3fd25ee76c2852d0fa7e152bc3dc95b
Tobbe <tobbe.kling@hotmail.com>
2016-04-17 12:58:21 +0200

    Added Observers and Entitys

--- SmallGameProject/SmallGameProject/Observer.h
+++ SmallGameProject/SmallGameProject/Observer.h
@@ -1,12 +1,17 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+#include "EventLib.h"
+class Entity;
+
 class Observer {
 
 private:
-
+	
 public:
-
+	Observer();
+	virtual ~Observer();
+	virtual void onNotify(const Entity* entity, Events::ENTITY evnt) = 0;
 };
 
 #endif
