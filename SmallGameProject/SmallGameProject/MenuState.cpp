5818fa439cd8a45d40d16b25dd515aabce4f6d3c
Pistonnoob <kimeld@hotmail.com>
2016-04-18 13:25:27 +0200

    MenuState + function definition

--- SmallGameProject/SmallGameProject/MenuState.cpp
+++ SmallGameProject/SmallGameProject/MenuState.cpp
@@ -10,3 +10,23 @@ MenuState::MenuState()
 MenuState::~MenuState()
 {
 }
+
+int MenuState::Initialize(ID3D11Device * device, ID3D11DeviceContext * deviceContext, GameStateHandler * GSH)
+{
+	return 0;
+}
+
+int MenuState::HandleInput(InputHandler * input)
+{
+	return 0;
+}
+
+int MenuState::Update(float deltaTime)
+{
+	return 0;
+}
+
+int MenuState::Render(GraphicHandler * gHandler, HWND hwnd)
+{
+	return 0;
+}
