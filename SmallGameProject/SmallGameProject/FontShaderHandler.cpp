899ccf5f93b3f7ff254def79b1b6be5ff6dc95b0
Oscar Engborg <Engborg@live.se>
2016-04-19 10:46:04 +0200

    Added the font engine

---
+++ SmallGameProject/SmallGameProject/FontShaderHandler.cpp
@@ -0,0 +1,300 @@
+#include "FontShaderHandler.h"
+
+FontShaderHandler::FontShaderHandler()
+{
+	this->vertexShader = nullptr;
+	this->pixelShader = nullptr;
+	this->layout = nullptr;
+	this->matrixBuffer = nullptr;
+	this->samplerState = nullptr;
+}
+
+FontShaderHandler::~FontShaderHandler()
+{
+}
+
+bool FontShaderHandler::Initialize(ID3D11Device* device)
+{
+	HRESULT hresult;
+	ID3D10Blob* errorMessage;
+	ID3D10Blob* vertexShaderBuffer;
+	ID3D10Blob* pixelShaderBuffer;
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[2];
+	unsigned int numElements;
+	D3D11_BUFFER_DESC matrixBufferDesc;
+	D3D11_SAMPLER_DESC samplerDesc;
+
+	//init pointers to nullptr
+	errorMessage = nullptr;
+	vertexShaderBuffer = nullptr;
+	pixelShaderBuffer = nullptr;
+
+	WCHAR* vsFilename = L"../SmallGameProject/FontVertexShader.hlsl";
+	WCHAR* psFilename = L"../SmallGameProject/FontPixelShader.hlsl";
+
+	//Compile the vertex shader code
+	hresult = D3DCompileFromFile(vsFilename, NULL, NULL, "main", "vs_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, &vertexShaderBuffer, &errorMessage);
+	if (FAILED(hresult)) {
+		if (errorMessage) {
+			OutputShaderErrorMessage(errorMessage, vsFilename);
+		}
+		return false;
+	}
+
+	//Compile the pixel shader code
+	hresult = D3DCompileFromFile(psFilename, NULL, NULL, "main", "ps_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, &pixelShaderBuffer, &errorMessage);
+	if (FAILED(hresult)) {
+		if (errorMessage) {
+			OutputShaderErrorMessage(errorMessage, psFilename);
+		}
+		return false;
+	}
+
+	//Create the vertex shader from buffer
+	hresult = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), NULL, &this->vertexShader);
+	if (FAILED(hresult)) {
+		return false;
+	}
+
+	//Create the pixel shader from buffer
+	hresult = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), pixelShaderBuffer->GetBufferSize(), NULL, &this->pixelShader);
+	if (FAILED(hresult)) {
+		return false;
+	}
+
+	//Fill the vertex input layout description 
+	//This needs to match the Model and shader 
+	polygonLayout[0].SemanticName = "POSITION";
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[0].InputSlot = 0;
+	polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	polygonLayout[1].SemanticName = "TEXCOORD";
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
+
+	//Get the number of elements in the layout
+	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+
+	//Create the vertex input layout.
+	hresult = device->CreateInputLayout(polygonLayout, numElements, vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), &this->layout);
+	if (FAILED(hresult)) {
+		return false;
+	}
+
+	//Release and nullptr the buffers as they are no longer needed
+	vertexShaderBuffer->Release();
+	vertexShaderBuffer = nullptr;
+	pixelShaderBuffer->Release();
+	pixelShaderBuffer = nullptr;
+
+	//Fill the description of the dynamic matrix constant buffer that is in the vertex shader
+	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	matrixBufferDesc.ByteWidth = sizeof(FontConstantBuffer);
+	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	matrixBufferDesc.MiscFlags = 0;
+	matrixBufferDesc.StructureByteStride = 0;
+
+	// Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class.
+	hresult = device->CreateBuffer(&matrixBufferDesc, NULL, &this->matrixBuffer);
+	if (FAILED(hresult)) {
+		return false;
+	}
+
+	//Fill the texture sampler state description
+	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.MipLODBias = 0.0f;
+	samplerDesc.MaxAnisotropy = 1;
+	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+	samplerDesc.BorderColor[0] = 0;
+	samplerDesc.BorderColor[1] = 0;
+	samplerDesc.BorderColor[2] = 0;
+	samplerDesc.BorderColor[3] = 0;
+	samplerDesc.MinLOD = 0;
+	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+	//Create the texture sampler state
+	hresult = device->CreateSamplerState(&samplerDesc, &this->samplerState);
+	if (FAILED(hresult))
+	{
+		return false;
+	}
+
+	D3D11_RENDER_TARGET_BLEND_DESC rtbd;
+	ZeroMemory(&rtbd, sizeof(rtbd));
+	rtbd.BlendEnable = true;
+	rtbd.SrcBlend = D3D11_BLEND_INV_SRC_ALPHA;
+	rtbd.DestBlend = D3D11_BLEND_SRC_ALPHA;
+	rtbd.BlendOp = D3D11_BLEND_OP_ADD;
+	rtbd.SrcBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA;
+	rtbd.DestBlendAlpha = D3D11_BLEND_SRC_ALPHA;
+	rtbd.BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	rtbd.RenderTargetWriteMask = D3D10_COLOR_WRITE_ENABLE_ALL;
+
+	D3D11_BLEND_DESC blendDesc;
+	ZeroMemory(&blendDesc, sizeof(blendDesc));
+	blendDesc.AlphaToCoverageEnable = false;
+	blendDesc.RenderTarget[0] = rtbd;
+
+	device->CreateBlendState(&blendDesc, &this->transparencyBlendState);
+
+	return true;
+}
+
+void FontShaderHandler::Shutdown()
+{
+	//Release sampler state
+	if (this->samplerState) {
+		this->samplerState->Release();
+		this->samplerState = nullptr;
+	}
+	//Release matrix constant buffer
+	if (this->matrixBuffer) {
+		this->matrixBuffer->Release();
+		this->matrixBuffer = nullptr;
+	}
+	//Release layout
+	if (this->layout) {
+		this->layout->Release();
+		this->layout = nullptr;
+	}
+	//Release pixel shader
+	if (this->pixelShader) {
+		this->pixelShader->Release();
+		this->pixelShader = nullptr;
+	}
+
+	//Release vertex shader
+	if (this->vertexShader) {
+		this->vertexShader->Release();
+		this->vertexShader = nullptr;
+	}
+
+	return;
+}
+
+
+bool FontShaderHandler::Render(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix,
+	XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* fontTexture, XMFLOAT3 color)
+{
+	bool result = false;
+
+	//Set shader parameters used for rendering
+	result = this->SetShaderParameters(deviceContext, worldMatrix, viewMatrix, projectionMatrix, fontTexture, color);
+	if (!result) {
+		return false;
+	}
+
+	this->RenderShader(deviceContext, indexCount);
+
+	return true;
+}
+
+void FontShaderHandler::OutputShaderErrorMessage(ID3D10Blob* errorMessage, WCHAR* shaderFilename)
+{
+	char* compileErrors;
+	unsigned long long bufferSize, i;
+	ofstream fout;
+
+	//Get a pointer to the error message text buffer
+	compileErrors = (char*)errorMessage->GetBufferPointer();
+
+	//Get the length of the message
+	bufferSize = errorMessage->GetBufferSize();
+
+	//Open a file to write error messages to
+	fout.open("shader_error.txt");
+
+	//Write the error message to the file
+	for (i = 0; i<bufferSize; i++)
+	{
+		fout << compileErrors[i];
+	}
+
+	//Close the file
+	fout.close();
+
+	//Release the error message
+	errorMessage->Release();
+	errorMessage = nullptr;
+
+	return;
+}
+
+bool FontShaderHandler::SetShaderParameters(ID3D11DeviceContext* deviceContext, XMMATRIX worldMatrix,
+	XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* fontTexture, XMFLOAT3 color)
+{
+	HRESULT hresult;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	FontConstantBuffer* dataPtr;
+	unsigned int bufferNumber;
+
+	//Transpose each matrix to prepare for shaders (requirement in directx 11)
+	worldMatrix = XMMatrixTranspose(worldMatrix);
+	viewMatrix = XMMatrixTranspose(viewMatrix);
+	projectionMatrix = XMMatrixTranspose(projectionMatrix);
+
+	//Map the constant buffer so we can write to it (denies GPU access)
+	hresult = deviceContext->Map(this->matrixBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	if (FAILED(hresult)) {
+		return false;
+	}
+
+	//Get pointer to the data
+	dataPtr = (FontConstantBuffer*)mappedResource.pData;
+
+	//Copy the matrices to the constant buffer
+	dataPtr->world = worldMatrix;
+	dataPtr->view = viewMatrix;
+	dataPtr->projection = projectionMatrix;
+	dataPtr->color = color;
+
+	//Unmap the constant buffer to give the GPU access agin
+	deviceContext->Unmap(this->matrixBuffer, 0);
+
+	//Set constant buffer position in vertex shader
+	bufferNumber = 0;
+
+	//Set the constant buffer in vertex and pixel shader with updated values
+	deviceContext->VSSetConstantBuffers(bufferNumber, 1, &this->matrixBuffer);
+	deviceContext->PSSetConstantBuffers(bufferNumber, 1, &this->matrixBuffer);
+
+	if (fontTexture) {
+		//Set shader color texture resource for pixel shader
+		deviceContext->PSSetShaderResources(0, 1, &fontTexture);
+	}
+
+	deviceContext->OMSetBlendState(this->transparencyBlendState, NULL, 0xffffffff);
+
+	return true;
+}
+
+void FontShaderHandler::RenderShader(ID3D11DeviceContext* deviceContext, int indexCount)
+{
+	//Set the input layout for vertex
+	deviceContext->IASetInputLayout(this->layout);
+
+	//Set the vertex and pixel shaders that will be used to render this triangle
+	deviceContext->VSSetShader(this->vertexShader, NULL, 0);
+	deviceContext->PSSetShader(this->pixelShader, NULL, 0);
+	deviceContext->GSSetShader(NULL, NULL, 0);
+
+	//Set the sampler state in pixel shader
+	deviceContext->PSSetSamplers(0, 1, &this->samplerState);
+
+	//Render the triangle
+	deviceContext->DrawIndexed(indexCount, 0, 0);
+
+	return;
+}
---
+++ SmallGameProject/SmallGameProject/TextHandler.cpp
@@ -0,0 +1,228 @@
+#include "TextHandler.h"
+
+
+TextHandler::TextHandler()
+{
+	this->baseViewMatrix = XMMatrixIdentity();
+	this->font = nullptr;
+	this->fontShaderH = nullptr;
+}
+
+TextHandler::TextHandler(const TextHandler& originalObj)
+{
+
+}
+
+TextHandler::~TextHandler()
+{
+
+}
+
+int TextHandler::CreateSentence(ID3D11Device* device, int maxLength)
+{
+	D3D11_BUFFER_DESC vertexBufferDesc;
+	D3D11_BUFFER_DESC indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData;
+	D3D11_SUBRESOURCE_DATA indexData;
+	HRESULT hresult;
+	bool result;
+	Sentence* tempSentence = new Sentence;
+
+	tempSentence->maxLength = maxLength; //Init the max character length of the sentence
+	tempSentence->vertexCount = tempSentence->indexCount = maxLength * 6; //Each letter has 6 vertices
+
+	Vertex* vertices = new Vertex[tempSentence->vertexCount]; //Create an empty vertex array and init to 0
+	memset(vertices, 0, (sizeof(Vertex) * tempSentence->vertexCount));
+
+	unsigned long* indices = new unsigned long[tempSentence->indexCount]; //Create and fill the index buffer
+	for (int i = 0; i < tempSentence->indexCount; i++) {
+		indices[i] = i;
+	}
+
+	//Set the description of the static vertex buffer
+	ZeroMemory(&vertexBufferDesc, sizeof(vertexBufferDesc));
+	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	vertexBufferDesc.ByteWidth = sizeof(Vertex) * tempSentence->vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	//Give the subresource structure a pointer to the vertex data
+	ZeroMemory(&vertexData, sizeof(vertexData));
+	vertexData.pSysMem = vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	//Create the vertex buffer
+	hresult = device->CreateBuffer(&vertexBufferDesc, &vertexData, &tempSentence->vertexBuffer);
+	if (FAILED(hresult)) {
+		return -1;
+	}
+
+	//Set up the description of the static index buffer
+	ZeroMemory(&indexBufferDesc, sizeof(indexBufferDesc));
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * tempSentence->indexCount;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	ZeroMemory(&indexData, sizeof(indexData));
+	indexData.pSysMem = indices;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	//Create the index buffer
+	hresult = device->CreateBuffer(&indexBufferDesc, &indexData, &tempSentence->indexBuffer);
+	if (FAILED(hresult)) {
+		return -1;
+	}
+
+	this->sentences.push_back(tempSentence); //Store the sentence obejct
+
+	delete[] vertices; //Delete no longer needed arrays
+	delete[] indices;
+
+	return this->sentences.size() - 1; //Return string id, starts from 0
+}
+
+bool TextHandler::UpdateSentence(ID3D11DeviceContext* deviceContext, int id, const std::string& text, int posX, int posY, const DirectX::XMFLOAT3& color)
+{
+	HRESULT result;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	Sentence* tempSentence = this->sentences.at(id); //Get the sentence by id
+	if (!tempSentence) {
+		return false;
+	}
+
+	int nrLetters = text.size();
+	if (nrLetters > tempSentence->maxLength) { //Check so the new text isn't longer then it can be
+		return false;
+	}
+
+	Vertex* vertices = new Vertex[tempSentence->vertexCount]; //Create an empty vertex array and init to 0
+	memset(vertices, 0, (sizeof(Vertex) * tempSentence->vertexCount));
+
+	float drawPosX = ((this->screenWidth / 2) * -1) + posX; //Calculate position in window to draw on
+	float drawPosY = (this->screenHeight / 2) - posY;
+
+	this->font->BuildVertexArray((void*)vertices, text.c_str(), drawPosX, drawPosY); //Ask the font object to build the vertex array
+
+	result = deviceContext->Map(tempSentence->vertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource); //Map the vertex buffer and discard it's content
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	Vertex* verticesPtr = (Vertex*)mappedResource.pData;
+
+	//Copy the new vertex data to buffer
+	memcpy(verticesPtr, vertices, (sizeof(Vertex) * tempSentence->vertexCount));
+
+	deviceContext->Unmap(tempSentence->vertexBuffer, 0);
+
+	delete[] vertices;
+
+	tempSentence->color = color;
+
+	return true;
+}
+
+void TextHandler::ReleaseSentences()
+{
+	for (int i = 0; i < this->sentences.size(); i++) {
+		if (this->sentences.at(i)) {
+			this->sentences.at(i)->ShutdownBuffers();
+			delete this->sentences.at(i);
+			this->sentences.at(i) = nullptr;
+		}
+	}
+
+	return;
+}
+
+void TextHandler::RenderSentence(ID3D11DeviceContext* deviceContext, int id)
+{
+	Sentence* tempSentence = this->sentences.at(id);
+	unsigned int stride;
+	unsigned offset;
+
+	//Set vertex buffer stride and offset
+	stride = sizeof(Vertex);
+	offset = 0;
+
+	//Set the vertex buffer to active in the input assembly so it can rendered
+	deviceContext->IASetVertexBuffers(0, 1, &tempSentence->vertexBuffer, &stride, &offset);
+
+	//Set the index buffer to active in the input assembler so it can be rendered
+	deviceContext->IASetIndexBuffer(tempSentence->indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	//Set the type od primitiv that should be rendered from this vertex buffer, in this case triangles
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	return;
+}
+
+bool TextHandler::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, XMMATRIX baseViewMatrix, int screenWidth, int screenHeight)
+{
+	bool result;
+
+	this->font = new Font;
+	if (!this->font) {
+		return false;
+	}
+	result = this->font->Initialize(device, deviceContext, "fontData.txt", "font.mtl");
+	if (!result) {
+		return false;
+	}
+
+	this->fontShaderH = new FontShaderHandler;
+	if (!this->fontShaderH) {
+		return false;
+	}
+	result = this->fontShaderH->Initialize(device);
+	if (!result) {
+		return false;
+	}
+
+	this->baseViewMatrix = baseViewMatrix;
+	this->screenWidth = screenWidth;
+	this->screenHeight = screenHeight;
+
+	return true;
+}
+
+void TextHandler::Shutdown()
+{
+	this->ReleaseSentences();
+
+	if (this->fontShaderH) {
+		this->fontShaderH->Shutdown();
+		delete this->fontShaderH;
+		this->fontShaderH = nullptr;
+	}
+
+	if (this->font) {
+		this->font->Shutdown();
+		delete this->font;
+		this->font = nullptr;
+	}
+
+	return;
+}
+
+void TextHandler::Render(ID3D11DeviceContext* deviceContext, XMMATRIX orthoMatrix)
+{
+	Sentence* tempSentence;
+	for (int i = 0; i < this->sentences.size(); i++) {
+		tempSentence = this->sentences.at(i);
+		this->RenderSentence(deviceContext, i); //Put the sentence vertex buffer on the pipeline
+
+												//Render the sentence
+		this->fontShaderH->Render(deviceContext, tempSentence->indexCount, XMMatrixIdentity(), this->baseViewMatrix, orthoMatrix, this->font->GetTexture(), tempSentence->color);
+	}
+
+	return;
+}
