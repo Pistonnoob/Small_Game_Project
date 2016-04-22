f4390485d665d3b14e8cd175e49def834822dc60
Oscar Engborg <Engborg@live.se>
2016-04-22 10:13:47 +0200

    Added some more camera functions

--- SmallGameProject/SmallGameProject/CameraHandler.cpp
+++ SmallGameProject/SmallGameProject/CameraHandler.cpp
@@ -17,8 +17,14 @@ bool CameraHandler::Initialize()
 	DirectX::XMVECTOR camUp = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 1.0f);
 	
 	this->viewMatrix = DirectX::XMMatrixLookAtLH(camPos, lookAt, camUp);
+	this->baseViewMatrix = this->viewMatrix;
 
 	return true;
+}
+
+void CameraHandler::GetBaseViewMatrix(DirectX::XMMATRIX & baseViewMatrix)
+{
+	baseViewMatrix = this->baseViewMatrix;
 }
 
 void CameraHandler::GetViewMatrix(DirectX::XMMATRIX& viewMatrix)
@@ -32,4 +38,9 @@ DirectX::XMFLOAT4 CameraHandler::GetCameraPos()
 	DirectX::XMStoreFloat4(&camPosFloat, this->cameraPos);
 
 	return camPosFloat;
+}
+
+void CameraHandler::SetCameraPos(DirectX::XMFLOAT3 newCamPos)
+{
+	this->cameraPos = DirectX::XMLoadFloat3(&newCamPos);
 }
