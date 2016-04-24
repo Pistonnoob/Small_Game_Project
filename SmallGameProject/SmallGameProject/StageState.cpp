745a32464d354079f708d5f9b403a55f2769d3b6
Pistonnoob <kimeld@hotmail.com>
2016-04-24 09:48:54 +0200

    StageState + implemented pure virtual

--- SmallGameProject/SmallGameProject/StageState.cpp
+++ SmallGameProject/SmallGameProject/StageState.cpp
@@ -4,9 +4,37 @@
 
 StageState::StageState()
 {
+	this->m_model = Model();
+	this->myCamera = CameraHandler();
 }
 
 
 StageState::~StageState()
 {
+	GameState::Shutdown();
+	this->m_model.Shutdown();
+}
+
+void StageState::Shutdown()
+{
+}
+
+int StageState::Initialize(ID3D11Device * device, ID3D11DeviceContext * deviceContext, GameStateHandler * GSH)
+{
+	return 0;
+}
+
+int StageState::HandleInput(InputHandler * input)
+{
+	return 0;
+}
+
+int StageState::Update(float deltaTime)
+{
+	return 0;
+}
+
+int StageState::Render(GraphicHandler * gHandler, HWND hwnd)
+{
+	return 0;
 }
--- SmallGameProject/SmallGameProject/StageState.h
+++ SmallGameProject/SmallGameProject/StageState.h
@@ -5,10 +5,20 @@ class StageState :
 	public GameState
 {
 private:	//Variables
+	Model m_model;
 
+	CameraHandler myCamera;
 public:
 	StageState();
 	virtual ~StageState();
+	void Shutdown();
+
+	int Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, GameStateHandler* GSH);
+
+	virtual int HandleInput(InputHandler* input);
+	virtual int Update(float deltaTime);
+	virtual int Render(GraphicHandler* gHandler, HWND hwnd);
+
 private:	//Functions
 
 };
