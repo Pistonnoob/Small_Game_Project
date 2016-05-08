53b5557aa916e7b86f47fa2e9f1908ac7b05f943
Oscar Engborg <Engborg@live.se>
2016-05-08 12:10:37 +0200

    Small changes

--- SmallGameProject/SmallGameProject/MenuState.cpp
+++ SmallGameProject/SmallGameProject/MenuState.cpp
@@ -54,7 +54,7 @@ int MenuState::Initialize(ID3D11Device * device, ID3D11DeviceContext * deviceCon
 		light.Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 		light.Ambient = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 		light.Specular = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-		light.Position = DirectX::XMFLOAT4(0.0f, 0.0f, -8.0f, 1.0f);
+		light.Position = DirectX::XMFLOAT4(0.0f, 0.0f, -10.0f, 1.0f);
 		light.Attenuation = DirectX::XMFLOAT4(50.0f, 1.0f, 0.09f, 0.032f);
 		this->pointLights.push_back(light);
 
