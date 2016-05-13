1339399e8ba353e95828ef61f141bddc164f084f
Pistonnoob <kimeld@hotmail.com>
2016-05-13 11:18:59 +0200

    Creates and updates SpawnEmitter

--- SmallGameProject/SmallGameProject/ParticleHandler.cpp
+++ SmallGameProject/SmallGameProject/ParticleHandler.cpp
@@ -53,9 +53,11 @@ void ParticleHandler::OnNotify(Entity * entity, Events::ENTITY evnt)
 		break;
 	case Events::PLAYER_CREATED:
 	{
-		newEmitter = new EmitterPlayerSpawn();
+		/*newEmitter = new EmitterPlayerSpawn();
 		newEmitter->Initialize(this->device, this->myTextures.GetTexture(0), 4.0f);
-		newEmitter->ApplyPosition(entity->GetPosition());
+		newEmitter->ApplyPosition(entity->GetPosition());*/
+		DirectX::XMFLOAT3 entityPosition = entity->GetPosition();
+		this->spawnEmitter.AddSpawnPulse(entityPosition.x, entityPosition.y, entityPosition.z, 0.2f, 4.0f, 0.1f, 0.8f, 0.8f, 3.0f);
 	}
 		break;
 	case Events::PLAYER_MOVING:
@@ -153,6 +155,7 @@ int ParticleHandler::Update(float dT, ID3D11DeviceContext * deviceContext)
 	{
  		(*emitter)->Update(dT, deviceContext);
 	}
+	this->spawnEmitter.Update(dT, deviceContext);
 	this->KillEmitters();
 	return result;
 }
