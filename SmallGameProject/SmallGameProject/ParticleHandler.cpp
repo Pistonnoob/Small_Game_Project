5b62783bf702a9511b8a20d9a86e9b2fcfece214
Pistonnoob <kimeld@hotmail.com>
2016-05-19 14:04:47 +0200

    menu stronger lighting

--- SmallGameProject/SmallGameProject/ParticleHandler.cpp
+++ SmallGameProject/SmallGameProject/ParticleHandler.cpp
@@ -170,7 +170,9 @@ void ParticleHandler::OnNotify(Entity * entity, Events::ENTITY evnt)
 		newEmitter = new EmitterClusterExplosion();
 		EmitterClusterExplosion* temp = (EmitterClusterExplosion*)newEmitter;
 		temp->Initialize(device, this->myTextures.GetTexture(0), 4.0f, 0.1f, 6, 0.1f);
-		temp->ApplyPosition(entity->GetPosition());
+		DirectX::XMFLOAT3 entityPosition = entity->GetPosition();
+		entityPosition.y += 1.5f;
+		temp->ApplyPosition(entityPosition);
 	}
 		break;
 	case Events::IDLE:
